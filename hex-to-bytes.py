a21de28dfd3e134f9dfd01c43cf78a1c4cd4270b
Leigh Morresi <dgtlmoon@gmail.com>
2015-05-25 15:19:32 +0200

    Updating usage comments

--- hex-to-bytes.py
+++ hex-to-bytes.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Simple script to convert list of hex bytes on STDIN to a binary file for testing
+# needs to be in the format of "0x01 0x02 0x03" etc
+
 import sys
 output =  open("output.jpeg", 'wb')
 for line in sys.stdin:
